58630eb84fab23c9448fc99a7565fce50c816b84
Karabo-Merinah <skosanamerinah@gmail.com>
2026-08-28 10:34:09 +0200

    Added case-sensetive for email for login and register .So it is automatically converted

--- src/service/UserAuthentication.ts
+++ src/service/UserAuthentication.ts
@@ -6,18 +6,19 @@ import { API_BASE_URL } from '../config/api'
 
 export async function setUserInfo(name:string,surname:string,email:string,cellnumber:string,password:string
 ){
-    const existing =await axios.get(`${API_BASE_URL}/users?email=${email}`)
+    const convertedEmail=email.toLowerCase()
+     const existing =await axios.get(`${API_BASE_URL}/users?email=${convertedEmail}`)
     if(existing.data.length >0){
         throw new Error("An account with this email already exists ")
     }
     const hashPassword= await bcrypt.hash(password,10)
-    const userInfo={name,surname,email,cellnumber,password:hashPassword}
+    const userInfo={name,surname,email:convertedEmail,cellnumber,password:hashPassword}
 
     const response=await axios.post(`${API_BASE_URL}/users`,userInfo)
     return response.data
 }
 export async function login(email:string,password:string){
-const response= await axios.get(`${API_BASE_URL}/users?email=${email}`)
+const response= await axios.get(`${API_BASE_URL}/users?email=${email.toLowerCase()}`)
 const users=response.data
 
 if(users.length === 0){
